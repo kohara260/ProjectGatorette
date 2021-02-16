d06b33d1dcd769b3e23feb4b7f6f00fba839fab6
Lucas Weaver <lucasweaver@ufl.edu>
2021-02-16 10:59:21 -0500

    add deep equals definition

--- server.test.js
+++ server.test.js
@@ -3,7 +3,29 @@ var should = require('should'),
     request = require('request');
 /* Globals */
 var listings;
+var deepEqual = function (x, y) {
+  if (x === y) {
+    return true;
+  }
+  else if ((typeof x == "object" && x != null) && (typeof y == "object" && y != null)) {
+    if (Object.keys(x).length != Object.keys(y).length)
+      return false;
 
+    for (var prop in x) {
+      if (y.hasOwnProperty(prop))
+      {  
+        if (! deepEqual(x[prop], y[prop]))
+          return false;
+      }
+      else
+        return false;
+    }
+
+    return true;
+  }
+  else 
+    return false;
+}
 /*
   Describe blocks organize your unit tests into distinct categories of functionality.
   
