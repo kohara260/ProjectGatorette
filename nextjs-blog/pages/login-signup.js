231fca6dd92c2104588e826f40021c7b3a7898df
Katriana O'Hara <kohara@ufl.edu>
2021-03-24 09:19:33 -0400

    Updated login/signup page by adding a background color and realigning the page's contents

--- nextjs-blog/pages/login-signup.js
+++ nextjs-blog/pages/login-signup.js
@@ -4,63 +4,82 @@ import Link from 'next/link'
 export default function Login() {
   return (
     <div className="container">
-        <Head>
-            <script type="text/javascript" src="/js/main.js"></script>
-        </Head>
-        <div className="left-logo">
-            <Link href="/"><img className="logo" className="logo" src="/logo.jpg" alt=""></img></Link>
-        </div>
-        <div className="box">
-            <div className="content columns">
-                <div className="content-tabs-row">
-                    <div className="row">
-                    <div className="content-tabs-row-inner">
-                     <button className="content-tab-activate active" id="button1" data-content-tab="overview">Login<i className="fas fa-caret-down small-margin-left"></i></button>
-                     <button className="content-tab-activate" id="button2" data-content-tab="representative-experience">Sign Up<i className="fas fa-caret-down small-margin-left"></i></button>
+        <div className="main">
+            <Head>
+                <script type="text/javascript" src="/js/main.js"></script>
+            </Head>
+            <div className="left-logo">
+                <Link href="/"><img className="logo" className="logo" src="/logo.jpg" alt=""></img></Link>
+            </div>
+            <div className="box">
+                <div className="content columns">
+                    <div className="content-tabs-row">
+                        <div className="row">
+                            <div className="content-tabs-row-inner">
+                                <button className="content-tab-activate active" id="button1" data-content-tab="overview">Login<i className="fas fa-caret-down small-margin-left"></i></button>
+                                <button className="content-tab-activate" id="button2" data-content-tab="representative-experience">Sign Up<i className="fas fa-caret-down small-margin-left"></i></button>
+                            </div>
+                        </div>
+                    </div>
+
+
+                    <div className="content-tab one active" data-content-tab="overview">
+                        <div>
+                            <form action="/loggedhome" className="form login">
+                                <label for="username">Username</label>
+                                <input type="text" name="username"></input>
+                                <label for="password">Password</label>
+                                <input type="password" name="password"></input>
+                                <input type="submit" value="Submit"></input>
+                            </form>
+                        </div>
+                    </div>
+
+                    <div className="content-tab two" data-content-tab="representative-experience">
+                            <form action="/loggedhome" className="form signup">
+                            <label for="fname">First Name</label>
+                                <input type="text" name="fname"></input>
+
+                                <label for="lname">Last Name</label>
+                                <input type="text" name="lname"></input>
+                                
+                                <label for="email">Email</label>
+                                <input type="email" name="email"></input>
+                                
+                                <label for="username2">Username</label>
+                                <input type="text" name="username2"></input>
+
+                                <label for="password2">Password</label>
+                                <input type="password" name="password2"></input>
+
+                                <input type="submit" value="Submit"></input>
+                            </form>
                     </div>
                 </div>
             </div>
-
-
-            <div className="content-tab one active" data-content-tab="overview">
-                <div>
-                    <form action="/loggedhome" className="form login">
-                        <label for="username">Username</label>
-                        <input type="text" name="username"></input>
-                        <label for="password">Password</label>
-                        <input type="password" name="password"></input>
-                        <input type="submit" value="Submit"></input>
-                    </form>
-                </div>
-            </div>
-
-            <div className="content-tab two" data-content-tab="representative-experience">
-                    <form action="/loggedhome" className="form signup">
-                    <label for="fname">First Name</label>
-                        <input type="text" name="fname"></input>
-
-                        <label for="lname">Last Name</label>
-                        <input type="text" name="lname"></input>
-                        
-                        <label for="email">Email</label>
-                        <input type="email" name="email"></input>
-                        
-                        <label for="username2">Username</label>
-                        <input type="text" name="username2"></input>
-
-                        <label for="password2">Password</label>
-                        <input type="password" name="password2"></input>
-
-                        <input type="submit" value="Submit"></input>
-                    </form>
-            </div>
-            </div>
         </div>
+
         <style jsx>{`
 
-        .box{
-            margin-top: 120px;
+        .main{
+            position: absolute;
+            top: 0;
+            bottom: 0;
+            left: 0;
+            right: 0;
+            height: 100%;
+            width: 100%;
+            background-color: #5CB3FF;
         }
+
+        .box{
+            margin: 0;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+
         .logo{
             max-width: 75px;
           }
@@ -88,11 +107,14 @@ export default function Login() {
             display: flex;
             flex-wrap: wrap;
         }
+
         label{
             margin: 15px 0 5px;
         }
+
         label, input{
-            width: 100%;
+            width: 80%;
+            // margin: auto;
         }
 
         input[type='submit']{
@@ -124,9 +146,8 @@ export default function Login() {
             background-color: #00529b;
             cursor: pointer;
         }
+
         `}</style>
     </div>
   )
 }
-
-
