5968f6e03afccde3988287bcf9d5b953db3e486c
kohara260 <61989597+kohara260@users.noreply.github.com>
2021-04-05 13:50:09 -0400

    Update main.js
    Still have not fixed issue in login/sign up page

--- nextjs-blog/public/js/main.js
+++ nextjs-blog/public/js/main.js
@@ -1,39 +1,35 @@
 console.log('testing');
 
-window.onload = function(){ 
-    //On click on button 1
-    document.getElementById("button1").onclick = function()
-    {
-     console.log('clicked button');
+window.onload = function() {
+   //On click on button 1
+   document.getElementById('button1').onclick = function(){
+      console.log('clicked button');
 
-     const el = document.querySelector('.content-tab.one');
-     const el2 = document.querySelector('.content-tab.two');
-     if (el.classList.contains("active")) 
-     {
-        console.log('content 1 is showing');
-     }
-     else{
-        console.log('content 2 was showing, switched to content 1');
-        el2.classList.remove("active");
-        el.classList.add("active");
-     }
-    } 
+      const el = document.querySelector('.content-tab.one');
+      const el2 = document.querySelector('.content-tab.two');
+      if (el.classList.contains('active')){
+         console.log('content 1 is showing');
+      }
+      else{
+         console.log('content 2 was showing, switched to content 1');
+         el2.classList.remove('active');
+         el.classList.add('active');
+      }
+   } 
     
-    //On click of button 2
-    document.getElementById("button2").onclick = function()
-    {
-     console.log('clicked button');
+   //On click of button 2
+   document.getElementById('button2').onclick = function(){
+      console.log('clicked button');
 
-     const el = document.querySelector('.content-tab.one');
-     const el2 = document.querySelector('.content-tab.two');
-     if (el2.classList.contains("active")) 
-     {
-        console.log('content 2 is showing');
-     }
-     else{
-        console.log('content 1 was showing, switched to content 2');
-        el.classList.remove("active");
-        el2.classList.add("active");
-     }
-    } 
+      const el = document.querySelector('.content-tab.one');
+      const el2 = document.querySelector('.content-tab.two');
+      if (el2.classList.contains('active')) {
+         console.log('content 2 is showing');
+      }
+      else{
+         console.log('content 1 was showing, switched to content 2');
+         el.classList.remove('active');
+         el2.classList.add('active');
+      }
+   } 
 };
